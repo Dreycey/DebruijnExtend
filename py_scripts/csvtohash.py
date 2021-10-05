a87f4595126272276414802cb2fef760368024bd
Dreycey <albindreycey@gmail.com>
2021-10-04 18:08:56 -0600

    updated and cleaned the csvtohash script

--- py_scripts/csvtohash.py
+++ py_scripts/csvtohash.py
@@ -1,134 +1,151 @@
 #! usr/bin/python3
-import numpy
+"""
+This module creates a hash table with sequences and the corresponding
+secondary structures.
+
+USAGE:
+    python csvtohash.py secondarystructure_COMP533.csv prothashtable.p
+
+OUTPUT:
+    GENERAL OUTPUT:
+        hashtable = { "sequence" : {"secondary structure" : counts} }   
+
+    SPECIFIC OUTPUT EXAMPLE:
+        hashtable = {"AGCTH" : {"CEEEH" : 2, ..., "EEEEE" : 30},
+                      ...
+                     "TTPGH" : {"HHHEE" : 34, ..., "HHEEE" : 2}}
+"""
+from typing import Dict
 import sys
 import pandas as pd
 from tqdm import tqdm
 import pickle
 
-"""
-This module creates a hash table with sequences and the corresponding
-secondary structures. Example:
 
-USAGE:
-python csvtohash.py secondarystructure_COMP533.csv prothashtable.p
 
-OUTPUT:
 
-                           GENERAL OUTPUT:
-hashtable = {                                                                   
-             "sequence" : {                                                        
-                           "secondary structure" : counts,                           
-                          }
-            }   
-
-                           SPECIFIC OUTPUT EXAMPLE:
-hashtable = {
-             "AGCTH" : {
-                        "CEEEH" : 2, 
-                        "EEEEE" : 30,  
-                       }
-             "TTPGH" : {                                                        
-                        "HHHEE" : 34,                                            
-                        "HHEEE" : 2,                                           
-                       } 
-             "WGGTT" : {                                                        
-                        "HHHHH" : 50,                                            
-                        "EEHHH" : 4,                                           
-                       }  
-            }
-
-"""
-
+HashTableType = Dict[str, Dict[str, float]]
 # class for the hash table
-class proteinhash:
+class ProteinHash:
     """
     This object holds the mappings from sequence to potential 
     secondary structure. This allows for a fast mapping of 
     sequence to structure for building a graph.
     """
     
-    def __init__(self, csv_in, k):
-        # define constructor variables.
-        self.csv_file = open(csv_in)
+    def __init__(self, csv_in, kmer_size):
+        self.csv_file: str = csv_in
         self.prothash = None # must construct on initialization
-        self.k_mersize = k # sliding window size for data structure
+        self.k_mersize = kmer_size 
+        self.sequence_column = 2
+        self.structure_column = 4
+        
+    def parse_csv(self, delimitor: str=",") -> pd.DataFrame:
+        """
+        This method parses an input csv and places the sequences
+        and corresponding secondary structures into a pnadas dataframe.
 
-        # construct the hash table
-        #self.construct_hash()
-        
-    def parse_csv(self):
+        Parameters
+        ----------
+        delimitor : str [DEFAULT: ","]
+            The delimitor to use per row of the input file.
+
+        Returns
+        -------
+        potential_secondaries: pd.DataFrame
+            Returns a dataframe connecting primary sequences with the 
+            corresponding secondary structure.
         """
-        This method parses an input csv and send the information into 
-        a seperate method which constructs the hash table.
-        """
-        file_by_lines = self.csv_file.readlines()
-        # construct the sequence and structure vectors from csv
-        sequence_vector = []
-        structure_vector = []
-        for line in file_by_lines:
-            line = line.split(",")
-            sequence_vector.append(line[2])
-            structure_vector.append(line[4])
-        # add sequences and structures to a pandas dataframe
-        df = {'sequence': sequence_vector, 'structure': structure_vector}
-        structseq_dataframe = pd.DataFrame(data=df)
-        
+        with open(self.csv_file) as opened_file:
+            file_by_lines = opened_file.readlines()
+            # construct the sequence and structure vectors from csv
+            sequence_vector = []
+            structure_vector = []
+            for csv_row in file_by_lines:
+                csv_row = csv_row.split(delimitor)
+                sequence_vector.append(csv_row[self.sequence_column])
+                structure_vector.append(csv_row[self.structure_column])
+            # add sequences and structures to a pandas dataframe
+            df = {'sequence': sequence_vector, 'structure': structure_vector}
+            structseq_dataframe = pd.DataFrame(data=df)
+
         return structseq_dataframe
     
-    def construct_hash(self):
+    def construct_hash(self, seq_column_name: str="sequence", 
+                             struct_column_name: str="structure") -> HashTableType:
         """
-        This meth creates a hash table.
+        This method creates the hash table used by the DebruijnExtend algorithm.
 
-        OUTPUT: returns a hashable datastructure.
+        Parameters
+        ----------
+        seq_column_name : str [DEFAULT: "sequence"]
+            The column name for the sequences in a pandas dataframe
+        struct_column_name : str [DEFAULT: "structure"]
+            The column name for the structures in a pandas dataframe
+
+        Returns
+        -------
+        potential_secondaries: HashTableType
+            returns a hashable datastructure.
         """
         prothash = {}
-        protein_df = self.parse_csv() # creates df needed for building hash table
-        k = self.k_mersize
-        
+        protein_df = self.parse_csv()
         # make the hash table
-        for index, row in tqdm(protein_df.iterrows()):
-            seq = row['sequence']
-            struct = row['structure']
-            for index in range(len(seq) - (k-1)):
-                start_pos = index
-                end_pos = index + k
-                seq_sub = seq[start_pos:end_pos]
-                struct_sub = struct[start_pos:end_pos]
-                if "*" not in seq_sub: # TODO: check what asterisk is!!!!!!!
-                    if seq_sub in prothash.keys():
-                        if struct_sub in prothash[seq_sub].keys():
-                            prothash[seq_sub][struct_sub] += 1
-                        else:
-                            prothash[seq_sub][struct_sub] = 1
-                    else:
-                        prothash[seq_sub] = {}
-                        prothash[seq_sub][struct_sub] = 1 
-            
+        for kmer_start_position, row in tqdm(protein_df.iterrows()):
+            primary_sequence, secondary_structure = row[seq_column_name], row[struct_column_name]
+            for kmer_start_position in range(len(primary_sequence) - (self.k_mersize-1)):
+                kmer_end_position = kmer_start_position + self.k_mersize
+                protein_seq_at_index = primary_sequence[kmer_start_position:kmer_end_position]
+                structure_seq_at_index = secondary_structure[kmer_start_position:kmer_end_position]
+                prothash = self.update_protein_hash_table(prothash,
+                                                          protein_seq_at_index,
+                                                          structure_seq_at_index)
         return prothash
+    
+    def update_protein_hash_table(self, prothash: HashTableType, 
+                                        protein_seq_at_index:str, 
+                                        structure_seq_at_index:str) -> HashTableType:
+        """
+        This method updates the protein hash table.
+        """
+        if "*" not in protein_seq_at_index: # TODO: check what asterisk is!!!!!!!
+            if protein_seq_at_index in prothash.keys():
+                if structure_seq_at_index in prothash[protein_seq_at_index].keys():
+                    prothash[protein_seq_at_index][structure_seq_at_index] += 1
+                else:
+                    prothash[protein_seq_at_index][structure_seq_at_index] = 1
+            else:
+                prothash[protein_seq_at_index] = {}
+                prothash[protein_seq_at_index][structure_seq_at_index] = 1   
+        return  prothash
 
-    def map(self, input_key):
+    def map(self, input_key: str) -> Dict[str, float]:
         """
         return the values for the input key.
 
-        INPUT: a sequence of length k, where k is the sliding window length
+        Parameters
+        ----------
+        input_key: str
+            a sequence of length k, where k is the sliding window length
+
+        Returns
+        -------
+        the potential secondary structures and associated counts.   
         """
         return self.prothash[input_key]
 
-
-
-# main for testing the hash object.
 def main():
     # defining the input arguments
     csvfile = sys.argv[1]
     outpickle_name = sys.argv[2] 
     kmer_size = int(sys.argv[3])
     # construct the hash table
-    prothashOBJ = proteinhash(csvfile, kmer_size)
+    prothashOBJ = ProteinHash(csvfile, kmer_size)
     prothashtable = prothashOBJ.construct_hash()
     print(prothashtable)
     # pickle the hash table
     outfile = open(outpickle_name,'wb')
-    pickle.dump(prothashtable,outfile)
+    pickle.dump(prothashtable, outfile)
 
 if __name__ == "__main__":
-    main();
+    main()
